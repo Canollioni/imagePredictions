727375c7d75c3c445526006346d1ba8faff05aac
Canollioni <48863385+Canollioni@users.noreply.github.com>
2019-03-23 20:39:34 -0400

    Update DataFitting_DecisionTree.py
    Using cross validation instead of ordinary training split, and am exploring the effect of different numbers of nodes for training.

--- DataFitting_DecisionTree.py
+++ DataFitting_DecisionTree.py
@@ -1,15 +1,16 @@
-# -*- coding: utf-8 -*-
 """
 Created on Thu Feb 21 17:57:44 2019
 
-@author: night
+@author: Canollioni
 """
 # Import function to create training and test set splits
 from sklearn.cross_validation import train_test_split
 
 from sklearn.metrics import mean_absolute_error
+from sklearn.pipeline import make_pipeline
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import cross_val_score
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -18,6 +19,18 @@ import time
 
 def getTimestamp(date):
     return time.mktime(dt.datetime.strptime(date,"%d/%m/%Y").timetuple())
+
+def get_mae_RF(max_LN, x, y):
+    pipeline_RF = make_pipeline(RandomForestRegressor(max_leaf_nodes = max_LN))
+    scores = cross_val_score(pipeline_RF, x, y, scoring="neg_mean_absolute_error")
+    mae = (-1 * scores.mean())
+    return(mae)
+
+def get_mae_DT(max_leaf_nodes, x, y):
+    pipeline_DT = make_pipeline(DecisionTreeRegressor(max_leaf_nodes = max_leaf_nodes))
+    scores = cross_val_score(pipeline_DT, x, y, scoring="neg_mean_absolute_error")
+    mae = (-1 * scores.mean())
+    return(mae)
 
 #Import data
 dataFilename = 'imageFeatures_190321.csv'
@@ -28,31 +41,42 @@ beginTS = getTimestamp("01/02/2019")
 logic_filt = imageDF.created>beginTS #(old_data.score>1000)# & (old_data.score<30000)
 filtered_data = imageDF[logic_filt]
 
-#Set variables
-x = filtered_data[feature].values
-
 imageModel_DT = DecisionTreeRegressor(random_state=1)
 imageModel_RF = RandomForestRegressor(random_state=1)
+
 
 #Select features to use for fit
 #feature_list = ['aspRatio', 'bluLRBal', 'bluTBBal', 'globalContrast', 'grnLRBal', 'grnTBBal', 'imgSz', 'lumLRBal', 'lumTBBal', 'luminance', 'redLRBal', 'redTBBal', 'saturation']
 feature_list = ['aspRatio', 'coarseContrast', 'globalContrast', 'imgSz', 'lumTBBal', 'luminance', 'saturation']
 
 
-
 #Select features and scores
 x = filtered_data[feature_list]
 y = filtered_data.score
-X_train, X_val, y_train, y_val = train_test_split(x, y,test_size=0.1)
+plt.hist(y)
+plt.show()
+#Normalize the data. Logically, higher scores are arbitrarily inflated due to higher visibility.
+#This is an attempted correction
+y = np.log(y)
+plt.hist(y)
+plt.show()
 
-imageModel_DT.fit(X_train,y_train)
-imageModel_RF.fit(X_train,y_train)
+maxNode = 100
 
-# get predicted prices on validation data
-val_predictions_DT = imageModel_DT.predict(X_val)
-print("Decision tree error:")
-print(mean_absolute_error(y_val, val_predictions_DT))
+MAE_RF = np.zeros(maxNode)
+MAE_DT = np.zeros(maxNode)
 
-val_predictions_RF = imageModel_RF.predict(X_val)
-print("Random forest error:")
-print(mean_absolute_error(y_val, val_predictions_RF))
+## compare MAE with differing values of max_leaf_nodes
+for max_leaf_nodes in range(2,maxNode):
+    MAE_DT[max_leaf_nodes] = np.exp(get_mae_DT(max_leaf_nodes, x, y))
+print(np.min(MAE_DT[2:]))
+plt.plot(MAE_DT[2:])
+plt.show() 
+
+    
+# compare MAE with differing values of max_leaf_nodes
+for max_leaf_nodes in range(2,maxNode):
+    MAE_RF[max_leaf_nodes] = np.exp(get_mae_RF(max_leaf_nodes, x, y))
+print(np.min(MAE_RF[2:]))
+plt.plot(MAE_RF[2:])
+plt.show() 
